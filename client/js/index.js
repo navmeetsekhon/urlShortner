7508b6db3971d7f3b1bf650ca72eeb72f91fec20
navmeetsekhon <navmeetsekhon@gmail.com>
2023-11-12 14:38:15 +0530

    client side fixed

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,42 @@
-function shortenUrl() {
-    var originalUrl = document.getElementById('originalUrl').value;
-    // You can implement the URL shortening logic here
-    // For simplicity, let's assume the shortened URL is a hash of the original URL
-    var shortenedUrl = '#' + Math.random().toString(36).substr(2, 6);
-    document.getElementById('shortenedUrl').innerHTML = '<strong>Shortened URL:</strong> ' + shortenedUrl;
-}
+const prefix='http://localhost:8080/v1/'
+async function postUrl(originalUrl) {
+    try {
+      const response = await fetch('http://localhost:8080/v1/createUrl', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Add any other headers as needed
+        },
+        body: JSON.stringify({
+          url: originalUrl,
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const responseData = await response.json();
+      return responseData.key; // Assuming the server returns the short URL key
+  
+    } catch (error) {
+      console.error('Error:', error);
+      throw error; // Rethrow the error to be caught by the calling function
+    }
+  }
+  
+async function shortenUrl() {
+    var originalUrl = document.getElementById("originalUrl").value;
+  
+    try {
+      const key = await postUrl(originalUrl);
+      console.log('Shortened URL:', shortUrl);
+      var shortUrl=prefix+key
+      document.getElementById("shortenedUrl").innerHTML =
+        "<strong>URL: </strong><a href='"+shortUrl+"'>" +shortUrl+"</a>";
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle errors if necessary
+    }
+  }
+  
